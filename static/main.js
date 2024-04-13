6461aed6201c201b96f05a2311575ef45b616ae3
stanleyctg <stanleyctg@yahoo.com>
2024-04-13 16:14:47 +0100

    Quiz function implemented

--- static/main.js
+++ static/main.js
@@ -1,14 +1,59 @@
 $(document).ready(function() {
+    let currentQuestionIndex = 0;
+    let questions = [];
+
+    // if (localStorage.getItem('currentQuestionIndex')) {
+    //     currentQuestionIndex = parseInt(localStorage.getItem('currentQuestionIndex'), 10);
+    // }
+
     $.ajax({
         url: "/get-questions",
         type: "POST",
         dataType: "json",
         success: function(response) {
-            var data = response.formatted_information[0].facts;  // Get the facts of the first question
-            $('#data-container').text(data);  // Display the facts in the data container
+            questions = response.formatted_information;
+            if (questions.length > 0) {
+                displayQuestion(currentQuestionIndex);
+            }
         },
         error: function(error) {
-            console.error('Error fetching data:', error);
+            console.error('Error fetching questions:', error);
         }
     });
+
+    function displayQuestion(questionIndex) {
+        const questionData = questions[questionIndex];
+        const qIndex = questionData[0];
+        const questionText = questionData[1];
+
+        // Set the question text
+        $('#question-title').text(qIndex + '. ' + questionText);
+        const correctAnswer = questionData[6]; // Correct answer at index 6
+
+        // Update answer texts and setup click handlers
+        ['answer1', 'answer2', 'answer3', 'answer4'].forEach((id, index) => {
+            const answer = questionData[index + 2];
+            const answerElement = $(`#${id}`);
+            answerElement.find('button').off('click').click(function() { checkAnswer(answer, correctAnswer); });
+            answerElement.find('span').text(answer); // Set text next to the button
+        });
+        if (questionIndex >= questions.length - 1) {
+            $('#next-question').hide(); // Hide the next button if it's the last question
+        } else {
+            $('#next-question').show(); // Otherwise, ensure it is visible
+        }
+    }
+
+    function checkAnswer(selectedAnswer, correctAnswer) {
+        alert(selectedAnswer === correctAnswer ? 'Correct!' : 'Wrong answer!');
+        if (currentQuestionIndex < questions.length - 1) {
+            currentQuestionIndex++;
+            localStorage.setItem('currentQuestionIndex', currentQuestionIndex);
+            displayQuestion(currentQuestionIndex);
+        } else {
+
+            alert('You have reached the end of the quiz!');
+            // Optionally handle quiz completion
+        }
+    }
 });
