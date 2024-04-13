41aa7ce4ff4c38b0f20b9d32355267ba7bab9539
stanleyctg <stanleyctg@yahoo.com>
2024-04-13 14:17:59 +0100

    Create get information in app.py

--- app.py
+++ app.py
@@ -1,11 +1,43 @@
-from flask import Flask, render_template
-import os
+from flask import Flask, render_template, jsonify
+import sqlite3
 
 app = Flask(__name__)
+quiz_db = "quiz.db"
+profile_db = "profile.db"
+
+def get_db_connection(database):
+    conn = sqlite3.connect(database)
+    return conn
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
+@app.route("/get-questions")
+def get_questions():
+    conn = sqlite3.connect('quiz.db')
+    cur = conn.cursor()
+    cur.execute("SELECT id, question, answers, correct_answer, facts FROM earth")
+    questions = cur.fetchall()
+    formatted_questions = []
+    for q in questions:
+        answers = q[2].split('|')
+        answer_dict = {chr(65 + i): answers[i] for i in range(len(answers))}  # Creates dict: {'A': 'First', 'B': 'Second', ...}
+        formatted_questions.append({
+            'id': q[0],
+            'question': q[1],
+            'answers': answer_dict,
+            'correct_answer': [k for k, v in answer_dict.items() if v == q[3]][0],  # Find the key where value matches the correct answer
+            'facts': q[4]
+        })
+    cur.close()
+    conn.close()
+
+    return jsonify(formatted_questions)
+
+
 if __name__ == "__main__":
+    get_questions()
     app.run(debug=True)
