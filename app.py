77fd2be5ed093e6e36b6072fcb8fb5d7b26f276d
j25224pl <pei.lee-3@student.manchester.ac.uk>
2024-04-13 16:37:10 +0100

    Set up HomePage

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, make_response, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
+    return render_template("layout.html")
+
+@app.route("/index")
+def index():
     return render_template("index.html")
+
+@app.route("/notes")
+def notes():
+    return render_template("notes.html")
+
+@app.route('/profile')
+def profile():
+    return render_template("profile.html")
+
+@app.route("/quiz")
+def quiz():
+    return render_template("quiz.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
