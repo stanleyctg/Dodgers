1119371184f86219e1261589ccd9e88ee9831d17
stanleyctg <stanleyctg@yahoo.com>
2024-04-15 15:37:06 +0100

    Add comments for main.js logic

--- static/main.js
+++ static/main.js
@@ -1,8 +1,10 @@
+// Handles the level page
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('.planet-btn').forEach(button => {
         button.addEventListener('click', function() {
             const planetNumber = this.getAttribute('data-planet');
             console.log("Planet number: ", planetNumber);
+            // Get the amount of fuel from flask
             $.ajax({
                 url: "/get_fuel",
                 type: "POST",
@@ -14,6 +16,7 @@ document.addEventListener('DOMContentLoaded', function() {
                     alert("Failed to fetch data.");
                 }
             });
+            // Determine which planet they are at and fetch data according to that plannet
             if ((planetNumber === '1') || (planetNumber === '2' && fuelAmount >= 100) || (planetNumber === '3' && fuelAmount >= 300)) {
                 fetchDataForPlanet(planetNumber);
             } else {
@@ -24,7 +27,9 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+// Get data accoring to the planet
 function fetchDataForPlanet(planetNumber) {
+    // Fetch data about planet from backend
     $.ajax({
         url: "/get-questions2",
         type: "POST",
@@ -32,6 +37,7 @@ function fetchDataForPlanet(planetNumber) {
         dataType: "json",
         success: function(response) {
             console.log('Data fetched for planet ' + planetNumber + ": ", response);
+            // Save the data so it can be use afterwards
             if (response.formatted_information) {
                 console.log('Formatted Information: ', response.formatted_information);
                 saveData(response.formatted_information);  // Pass data directly to saveData
@@ -44,8 +50,10 @@ function fetchDataForPlanet(planetNumber) {
 }
 
 
+// Save the data of the planet
 function saveData(formattedInfo){
     console.log("Saving data: ", formattedInfo);
+    // Connects to the backend and save data in sqlite3
     $.ajax({
         url: "/save_data",
         type: "POST",
@@ -53,7 +61,7 @@ function saveData(formattedInfo){
         data: JSON.stringify({ "formatted_info": formattedInfo }),
         dataType: "json",
         success: function(response) {
-
+            // Goes to the /notes route
             window.location.href = '/notes';
         },
         error: function(error) {
@@ -64,9 +72,11 @@ function saveData(formattedInfo){
 }
 
 
+// Handle the flashcard movements
 $(document).ready(function() {
-    var currentIndex = 0; // Initialize the index to 0
-    var facts = []; // Array to store all the facts
+    // Initialise the index, and facts
+    var currentIndex = 0;
+    var facts = [];
 
     // Function to fetch data from the server
     function fetchData() {
@@ -77,6 +87,7 @@ $(document).ready(function() {
             success: function(response) {
                 // alert(response.stored_data)
                 facts = response.stored_data;
+                // Format the facts
                 facts = parseQuestionString(facts)
                 displayFact(currentIndex);
             },
@@ -87,6 +98,7 @@ $(document).ready(function() {
     }
 
     function parseQuestionString(questionString) {
+        // Change the format of the questionString so it works for the movement and accessing of index
         var items = questionString.split(',');
         var questions = [];
         var temp = [];
@@ -107,10 +119,12 @@ $(document).ready(function() {
     }
     
 
+    // Display the fact (flashcard)
     function displayFact(index) {
         if (index < facts.length && facts[index].length > 7) {
-            var fact = facts[index][7]; // Make sure this index exists
-            $('#data-container').text(fact); // Display the fact
+            // The fact exists at index 7
+            var fact = facts[index][7];
+            $('#data-container').text(fact);
         } else {
             $('#data-container').text("No detail available for this fact.");
         }
@@ -121,10 +135,11 @@ $(document).ready(function() {
     $('#next-btn').click(function() {
          // Increment the index
         if (currentIndex >= facts.length - 1) {
-            alert("This is the end of the stack"); // Reset index to loop back to the first fact
+            alert("This is the end of the stack");
         }else{
+             // Display the next fact  
             currentIndex++;
-            displayFact(currentIndex); // Display the next fact            
+            displayFact(currentIndex);          
         }
 
     });
@@ -134,10 +149,10 @@ $(document).ready(function() {
     $('#prev-btn').click(function() {
          // Decrement the index
         if (currentIndex === 0) {
-            alert("This is the start of the stack") // Set index to the last fact if it goes below 0
+            alert("This is the start of the stack");
         }else{
             currentIndex--;
-            displayFact(currentIndex); // Display the previous fact            
+            displayFact(currentIndex);         
         }
 
     });
@@ -147,19 +162,22 @@ $(document).ready(function() {
 });
 
 
+// Handles the movement and logic of questions
 $(document).ready(function() {
+    // Initialise question index, questions and score
     let currentQuestionIndex = 0;
     let questions = [];
-    let score = 0; // Initialize score
+    let score = 0;
 
+    // Fetch the data from server to get the information about the planet
     function fetchData() {
         $.ajax({
             url: "/retrieve_data",
             type: "POST",
             dataType: "json",
             success: function(response) {
-                // alert(response.stored_data)
                 questions = response.stored_data;
+                // Same as before call the function to format the questions
                 questions = parseQuestionString2(questions);
                 if (questions.length > 0) {
                     displayQuestion(currentQuestionIndex);
@@ -169,12 +187,12 @@ $(document).ready(function() {
                 
             }
         });
+        // Fetch the fuel data then initialise it
         $.ajax({
             url: "/get_fuel",
             type: "POST",
             dataType: "json",
             success: function(response) {
-                // alert(response.stored_data)
                 fuel = parseInt(response.fuel);
             },
             error: function(error) {
@@ -183,6 +201,7 @@ $(document).ready(function() {
         });
     }
 
+    // Function to convert the questionString so it can be accessible by index
     function parseQuestionString2(questionString) {
         var items = questionString.split(',');
         var ques = [];
@@ -204,15 +223,20 @@ $(document).ready(function() {
     }
     
 
+    // Display the questions
     function displayQuestion(questionIndex) {
         const questionData = questions[questionIndex];
+        // Questions are at index 1
         $('#question-title').text((questionIndex + 1) + '. ' + questionData[1]);
-        const correctAnswer = questionData[6]; // Correct answer at index 6
+        // Correct answers are at index 6
+        const correctAnswer = questionData[6];
+        // The fact is at index 7
         const fact = questionData[7];
 
         ['answer1', 'answer2', 'answer3', 'answer4'].forEach((id, index) => {
             const answer = questionData[index + 2];
             const answerElement = $(`#${id}`);
+            // When the answer button is pressed calls the check answer function
             answerElement.find('button').off('click').click(function() { 
                 checkAnswer(answer, correctAnswer, fact); 
             });
@@ -222,32 +246,37 @@ $(document).ready(function() {
         $('#next-question').toggle(questionIndex < questions.length - 1);
     }
 
+    // Function to check answer
     function checkAnswer(selectedAnswer, correctAnswer, fact) {
+        // If correct, increment the score and update fuel
         if (selectedAnswer === correctAnswer) {
             score++;
             var element = document.querySelector('#\\#score-quiz');
-            element.innerHTML = 'Score: '+ score;  // For HTML content
+            element.innerHTML = 'Score: '+ score;
             alert("Correct");
             updateFuel();
-            
+        
         } else {
             alert("Incorrect!\n" + fact);
         }
 
+        // If there are no more questions, alert user and let them know
         if (currentQuestionIndex < questions.length - 1) {
             currentQuestionIndex++;
-            localStorage.setItem('currentQuestionIndex', currentQuestionIndex);
             displayQuestion(currentQuestionIndex);
         } else {
             alert('You have reached the end of the quiz!');
             $('#next-question').hide();
+            // Goes back to level page
             window.location.href = '/level';
         }
     }
     
-
+    // Function to update fuel
     function updateFuel(){
+        // Adds ten
         fuel = fuel+ 10;
+        // Connect to the server and update fuel to the database
         $.ajax({
             url: "/update_fuel",
             type: "POST",
@@ -262,6 +291,7 @@ $(document).ready(function() {
             }
         });
     }
+    // When document loaded, fetch content
     fetchData();
 })
 
